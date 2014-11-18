23b6329b388634851f7f0a002f7ae2d7d9620ca8
DavidJohnGee <david.gee@ipengineer.net>
2014-11-18 14:18:36 +0000

    Modified Setup
    Signed-off-by: DavidJohnGee <david.gee@ipengineer.net>

--- setup.py
+++ setup.py
@@ -13,10 +13,8 @@
 # limitations under the License.
 
 from setuptools import setup, find_packages
-from distutils.command.install import install as _install
 
 import sys
-import platform
 
 if not sys.version_info[0] == 2:
     print "Sorry, Python 3 is not supported (yet)"
